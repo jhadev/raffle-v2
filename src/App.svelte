747db3404b4d9a23eac3a67736a77bb67b1932d7
jhadev <joshappeldev@gmail.com>
2019-07-12 21:48:02 -0400

    column component

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,29 @@
   import EntryForm from "./EntryForm.svelte";
   import Container from "./common/Container.svelte";
   import Jumbotron from "./common/Jumbotron.svelte";
+  import Column from "./common/Column.svelte";
   import Row from "./common/Row.svelte";
   let raffle = [1, 3, 4];
   let name = "Josh";
   let entries = 0;
+  let colSize = 6;
 </script>
 
 <style>
   /* i don't have much style yet */
+  h1 {
+    text-align: center;
+    color: aliceblue;
+  }
 </style>
 
 <Container>
   <Jumbotron textCenter>
     <h1>Hello {name}!</h1>
-    <EntryForm {raffle} {name} {entries} />
   </Jumbotron>
   <Row center>
-    <h1>Test</h1>
+    <Column mobile={12} md={6}>
+      <EntryForm {raffle} {name} {entries} />
+    </Column>
   </Row>
 </Container>
---
+++ src/common/Column.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let mobile;
+  export let md;
+  export let lg = "";
+  export let xl = "";
+</script>
+
+<div class={`col-${mobile} col-md-${md} col-lg-${lg} col-xl-${xl}`}>
+  <slot />
+</div>
