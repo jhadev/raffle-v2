73a0bb4a69b153642e8e0c6492e9ef5526e71265
jhadev <joshappeldev@gmail.com>
2019-07-13 13:00:34 -0400

    created input component and moved logic to parent for now

---
+++ src/common/Input.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let type = "input";
+  export let id;
+  export let label;
+  export let placeholder = "";
+  export let inputType;
+  export let rows = 3;
+  export let value;
+  export let className = "";
+</script>
+
+<style>
+  /* no style yet */
+</style>
+
+<div class="form-group">
+  <label for={id}>{label}</label>
+
+  {#if type === 'textarea'}
+    <textarea
+      class={!className ? `form-control` : `form-control ${className}`}
+      {rows}
+      {id}
+      {value}
+      on:input />
+  {:else}
+    <input
+      type={inputType}
+      class={!className ? `form-control` : `form-control ${className}`}
+      {id}
+      {value}
+      on:input
+      aria-describedby={id}
+      {placeholder} />
+  {/if}
+</div>
