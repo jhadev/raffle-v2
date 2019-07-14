c9d0860185594549803b5d1ed19a5d14330cbd3b
jhadev <joshappeldev@gmail.com>
2019-07-13 21:21:56 -0400

    extracted display from app into its own component

---
+++ src/Display.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let count;
+</script>
+
+<style>
+  .odds {
+    font-size: 1.2rem;
+  }
+
+  .count-wrapper {
+    padding: 1rem 1.5rem;
+    margin: 0rem 1rem 1rem 1rem;
+  }
+</style>
+
+<div class="text-light text-center">
+  <!-- iterate over count declaration -->
+  {#each count as { name, total, odds }}
+    <div
+      class="count-wrapper d-flex justify-content-between border border-light">
+      <!-- delete button -->
+      <button id={name} on:click class="btn btn-outline-light">X</button>
+      <!-- display content -->
+      <div class="align-self-center badge badge-light odds">
+        {name}: {total}
+      </div>
+
+      {#if odds > 50}
+        <div class="align-self-center odds badge badge-success">{odds}%</div>
+      {:else if odds < 10}
+        <div class="align-self-center odds badge badge-danger">{odds}%</div>
+      {:else}
+        <div class="align-self-center odds badge badge-light">{odds}%</div>
+      {/if}
+    </div>
+  {:else}
+    <h2>Counts go here</h2>
+  {/each}
+</div>
